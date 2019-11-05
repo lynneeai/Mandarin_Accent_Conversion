f9cb8254155e50ebabeab1eea4efeb359f956abb
Tallis Jeng <alog1024@gmail.com>
2019-11-04 22:32:17 -0500

    fix indent

--- speex_sh/install_speex.sh
+++ speex_sh/install_speex.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 cd $HOME
 if [[ ! -e project ]]; then
-	    mkdir project
-    fi
-    cd project
-    git clone https://github.com/xiph/speex.git   
-    cd speex
-    sudo apt-get install autoconf
-    sudo apt-get install libtool
-    sudo apt install make
-    sudo apt-get install build-essential libspeex-dev libspeexdsp-dev libpulse-dev
-    bash autogen.sh
-    bash configure --prefix=$HOME
-    make
-    sudo make install
+    mkdir project
+fi
+cd project
+git clone https://github.com/xiph/speex.git   
+cd speex
+sudo apt-get install autoconf
+sudo apt-get install libtool
+sudo apt install make
+sudo apt-get install build-essential libspeex-dev libspeexdsp-dev libpulse-dev
+bash autogen.sh
+bash configure --prefix=$HOME
+make
+sudo make install
--- speex_sh/wav_to_spx.sh
+++ speex_sh/wav_to_spx.sh
@@ -2,38 +2,37 @@
 
 helpFunction()
 {
-	   echo ""
-	      echo "Usage: $0 -w waveList -s speexDir"
-	         echo -e "\t-w waveList is path to the file containing path to the wav files to be speex-encoded"
-		    echo -e "\t-s speexDir is path to the directory where output speex files will be written"
-		       exit 1 # Exit script after printing help
-	       }
+   echo ""
+   echo "Usage: $0 -w waveList -s speexDir"
+   echo -e "\t-w waveList is path to the file containing path to the wav files to be speex-encoded"
+   echo -e "\t-s speexDir is path to the directory where output speex files will be written"
+   exit 1 # Exit script after printing help
+}
 
-	       while getopts "w:s:" opt
-	       do
-		          case "$opt" in
-				        w ) waveList="$OPTARG" ;;
-					      s ) speexDir="$OPTARG" ;;
-					            ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
-						       esac
-					       done
+while getopts "w:s:" opt
+do
+   case "$opt" in
+      w ) waveList="$OPTARG" ;;
+      s ) speexDir="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
 
-					       # some parameters are empty, print helpFunction
-					       if [ -z "$waveList" ] || [ -z "$speexDir" ]
-					       then
-						          echo "Some or all of the parameters are empty";
-							     helpFunction
-						     fi
+# some parameters are empty, print helpFunction
+if [ -z "$waveList" ] || [ -z "$speexDir" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
 
-						     # all parameters are correct, do the job
-						     echo "speex-encoding wav files from $waveList"
-						     cat $waveList | while read line || [[ -n $line ]]; # $line is a wav file
-						     #filename="hi"
-					     do
-						        filename=`echo "$line" | sed -r "s/.+\/(.+)\..+/\1/"`
-							   echo $filename
-							      speexenc $line "$speexDir/$filename.spx" #speexenc input_file.wav compressed_file.spx
-						      done
-						      echo "encoded speex files written to $speexDir"
-
+# all parameters are correct, do the job
+echo "speex-encoding wav files from $waveList"
+cat $waveList | while read line || [[ -n $line ]]; # $line is a wav file
+#filename="hi"
+do
+   filename=`echo "$line" | sed -r "s/.+\/(.+)\..+/\1/"`
+   echo $filename
+   speexenc $line "$speexDir/$filename.spx" #speexenc input_file.wav compressed_file.spx
+done
+echo "encoded speex files written to $speexDir"
 
