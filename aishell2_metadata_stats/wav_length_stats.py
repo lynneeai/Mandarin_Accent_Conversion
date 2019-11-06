fc4934f648fd28e04c3d5e8bc36b5352c89b7ebb
lynneeai <lynneeai@hotmail.com>
2019-11-05 21:46:50 -0500

    wav length stats

--- aishell2_metadata_stats/wav_length_stats.py
+++ aishell2_metadata_stats/wav_length_stats.py
@@ -1,4 +1,5 @@
 import os
+import random
 import parselmouth
 import numpy as np
 import pandas as pd
@@ -7,24 +8,41 @@ import matplotlib.pylab as plt
 from collections import defaultdict
 from tqdm import tqdm
 
-ROOT_DIR = '../aishell_2/data'
+ROOT_DIR = '../aishell_2_partitions'
 
-time_list = []
-with open(f'{ROOT_DIR}/wav.scp', 'r') as infile:
+def random_k_items(l, k, seed):
+	list_length = len(l)
+	if k > list_length:
+		raise Exception('k should be smaller than list length')
+	idx = set()
+	random.seed(seed)
+	while len(idx) < k:
+		idx.add(random.randint(0, list_length - 1))
+	return [l[i] for i in idx]
+
+
+total_lines = []
+with open(f'{ROOT_DIR}/class_train.txt', 'r') as infile:
+	counter = 1
 	for line in infile:
-		_, wav_file = line.split()
-		sound = parselmouth.Sound(f'{ROOT_DIR}/{wav_file}')
-		time = float(sound.get_total_duration())
-		time_list.append(time)
+		total_lines.append(line)
+
+selected_lines = random_k_items(total_lines, 5000, 123)
+time_list = []
+for line in tqdm(selected_lines):
+	wav_file, _ = line.split()
+	sound = parselmouth.Sound(f'{wav_file}')
+	time = float(sound.get_total_duration())
+	time_list.append(time)
 
 ax = sns.distplot(time_list)
 plt.tight_layout()
-plt.show()
+plt.savefig('wav_length_dist.png')
 
 time_list = np.array(time_list)
 with open('wav_length_stats.txt', 'w') as outfile:
-	outfile.writelines(f'mean value: {np.mean(time_list)}')
-	outfile.writelines(f'min value: {np.min(time_list)}')
-	outfile.writelines(f'max value: {np.max(time_list)}')
+	outfile.writelines(f'mean value: {np.mean(time_list)}\n')
+	outfile.writelines(f'min value: {np.min(time_list)}\n')
+	outfile.writelines(f'max value: {np.max(time_list)}\n')
 	outfile.writelines(f'80 percentile: {np.percentile(time_list, 90)}\n')
 	outfile.writelines(f'90 percentile: {np.percentile(time_list, 90)}\n')
